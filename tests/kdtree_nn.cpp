55ccf75531e2d22289f7d587a079e2bd5df1cb95
samridh-dev <singhsam@grinnell.edu>
2025-02-08 15:38:54 -0600

    Update tests/kdtree_nn.cpp

--- tests/kdtree_nn.cpp
+++ tests/kdtree_nn.cpp
@@ -21,82 +21,209 @@
 
 #include "pch.h"
 
-#include <layout.hpp>
+#include <create/create.hpp>
 #include <nn.hpp>
 
 TEST_CASE("[basic_example] kdtree::nn") {
 
-    constexpr kdtree::layout maj = kdtree::layout::rowmajor;
-    constexpr uint8_t        dim = 2;
-    constexpr std::size_t    n   = 10;
+  using type_v = int;
+  using type_s = std::size_t;
 
-    std::vector<int> set = {
-      46, 63,
-      15, 43,
-      53, 67,
-      40, 33,
-      44, 58,
-      68, 21,
-      62, 69,
-      10, 15,
-      45, 40,
-      25, 54,
-    };
+  constexpr type_s dim = 2;
+  constexpr type_s n   = 10;
 
-    SUBCASE("q={11,12}") {
+  kdtree::context ctx;
 
-      std::vector<int> q{11, 12};
-      std::vector<int> ans{10, 15};
+  std::vector<type_v> vec = {
+    10, 15,
+    46, 63,
+    68, 21,
+    40, 33,
+    25, 54,
+    15, 43,
+    44, 58,
+    45, 40,
+    62, 69,
+    53, 67,
+  };
 
-      const auto i = kdtree::nn<int8_t, int, dim, maj>(q, set, n);
+  kdtree::create<type_s, dim>(ctx, vec, n);
 
-      for (int j = 0; j < dim; ++j) CHECK(set[dim * i + j] == ans[j]);
-
-    }
-
-  SUBCASE("q={50, 65}") {
-    std::vector<int> q{50, 65};
-    std::vector<int> ans{53, 67};
-
-    const auto i = kdtree::nn<int8_t, int, dim, maj>(q, set, n);
-
-    for (int j = 0; j < dim; ++j) CHECK(set[dim * i + j] == ans[j]);
+  SUBCASE("q = {1, 1}") {
+    std::vector<type_v> q   { 1, 1 };
+    std::vector<type_v> ans { 10, 15 };
+    const auto idx = kdtree::nn<int, int, dim>(ctx, q, vec, n);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 0] == ans[0]);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 1] == ans[1]);
   }
 
-  SUBCASE("q={20, 50}") {
-    std::vector<int> q{20, 50};
-    std::vector<int> ans{25, 54};
-
-    const auto i = kdtree::nn<int8_t, int, dim, maj>(q, set, n);
-
-    for (int j = 0; j < dim; ++j) CHECK(set[dim * i + j] == ans[j]);
+  SUBCASE("q equals existing point {46, 63}") {
+    std::vector<type_v> q   { 46, 63 };
+    std::vector<type_v> ans { 46, 63 };
+    const auto idx = kdtree::nn<int, int, dim>(ctx, q, vec, n);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 0] == ans[0]);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 1] == ans[1]);
   }
 
-  SUBCASE("q={60, 70}") {
-    std::vector<int> q{60, 70};
-    std::vector<int> ans{62, 69};
-
-    const auto i = kdtree::nn<int8_t, int, dim, maj>(q, set, n);
-
-    for (int j = 0; j < dim; ++j) CHECK(set[dim * i + j] == ans[j]);
+  SUBCASE("q = {50, 50}") {
+    std::vector<type_v> q   { 50, 50 };
+    std::vector<type_v> ans { 44, 58 };
+    const auto idx = kdtree::nn<int, int, dim>(ctx, q, vec, n);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 0] == ans[0]);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 1] == ans[1]);
   }
 
-  SUBCASE("q={39, 32}") {
-    std::vector<int> q{39, 32};
-    std::vector<int> ans{40, 33};
-
-    const auto i = kdtree::nn<int8_t, int, dim, maj>(q, set, n);
-
-    for (int j = 0; j < dim; ++j) CHECK(set[dim * i + j] == ans[j]);
+  SUBCASE("q = {70, 20}") {
+    std::vector<type_v> q   { 70, 20 };
+    std::vector<type_v> ans { 68, 21 };
+    const auto idx = kdtree::nn<int, int, dim>(ctx, q, vec, n);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 0] == ans[0]);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 1] == ans[1]);
   }
 
-  SUBCASE("q={47, 60}") {
-    std::vector<int> q{47, 60};
-    std::vector<int> ans{46, 63};
+  SUBCASE("q = {48, 45}") {
+    std::vector<type_v> q   { 48, 45 };
+    std::vector<type_v> ans { 45, 40 };
+    const auto idx = kdtree::nn<int, int, dim>(ctx, q, vec, n);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 0] == ans[0]);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 1] == ans[1]);
+  }
 
-    const auto i = kdtree::nn<int8_t, int, dim, maj>(q, set, n);
-
-    for (int j = 0; j < dim; ++j) CHECK(set[dim * i + j] == ans[j]);
+  SUBCASE("q = {100, 100}") {
+    std::vector<type_v> q   { 100, 100 };
+    std::vector<type_v> ans { 62, 69 };
+    const auto idx = kdtree::nn<int, int, dim>(ctx, q, vec, n);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 0] == ans[0]);
+    CHECK(vec[static_cast<std::size_t>(idx) * dim + 1] == ans[1]);
   }
 
 }
+
+namespace ref {
+
+template<typename F, typename T, T dim,
+         kdtree::container::layout maj = kdtree::container::layout::row_major,
+         typename C_query, typename C_tree> 
+requires kdtree::container::container_1d<C_query> 
+      && kdtree::container::container<C_tree>
+      && std::is_integral_v<T>
+      && std::is_arithmetic_v<F>
+      && std::is_same_v<kdtree::container::get_primitive_t<C_query>,
+                        kdtree::container::get_primitive_t<C_tree>>
+static T
+nn(const kdtree::context& ctx, const C_query& q, const C_tree& tree, 
+   const T n, F rmax = std::numeric_limits<F>::max()) {
+
+  (void) ctx;
+
+  using kdtree::internal::dist::euclidian;
+
+  T best_idx  {0};
+  F best_dist {rmax};
+
+  for (T i{0}; i < n; ++i) {
+
+    F dst{
+      euclidian<F, T, dim, maj, C_query, maj, C_tree>(q, 1, 0, tree, n, i)
+    };
+
+    if (dst < best_dist) {
+      best_dist = dst;
+      best_idx = i;
+    }
+
+  }
+  
+  return best_idx;
+}
+
+
+}
+
+#include <random>
+template <typename C>
+void 
+generate_random_dataset(C& v) {
+
+  using T = typename C::value_type;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<T> dist(std::numeric_limits<T>::min(),
+                                        std::numeric_limits<T>::max());
+
+  for (auto& i : v) i = dist(gen);
+
+}
+
+
+template <std::size_t dim, kdtree::container::layout maj, std::size_t n>
+void 
+test_nn_impl(void) {
+  constexpr std::size_t imax = 1e1;
+  using type_v = int;
+  using type_s = std::size_t;
+
+  kdtree::context ctx;
+
+  std::vector<type_v> vec(dim * n);
+  generate_random_dataset(vec);
+
+  kdtree::create<type_s, dim>(ctx, vec, n);
+
+  for (std::size_t i = 0; i < imax; ++i) {
+    SUBCASE(("nn check, i=" + std::to_string(i)).c_str()) {
+      std::vector<type_v> q(dim);
+      generate_random_dataset(q);
+
+      const auto idx = kdtree::nn<double, int, dim, maj>(ctx, q, vec, n);
+      const auto ans = ref::nn<double, int, dim, maj>(ctx, q, vec, n);
+      CHECK(idx == ans);
+    }
+  }
+}
+
+TEST_CASE("[random][case=1] kdtree::nn dim=1 n=1<<6") {
+  test_nn_impl<1, kdtree::container::layout::row_major, 1 << 6>();
+  test_nn_impl<1, kdtree::container::layout::col_major, 1 << 6>();
+}
+
+TEST_CASE("[random][case=1] kdtree::nn dim=2 n=1<<6") {
+  test_nn_impl<2, kdtree::container::layout::row_major, 1 << 6>();
+  test_nn_impl<2, kdtree::container::layout::col_major, 1 << 6>();
+}
+
+TEST_CASE("[random][case=1] kdtree::nn dim=3 n=1<<6") {
+  test_nn_impl<3, kdtree::container::layout::row_major, 1 << 6>();
+  test_nn_impl<3, kdtree::container::layout::col_major, 1 << 6>();
+}
+
+TEST_CASE("[random][case=1] kdtree::nn dim=3 n=1<<6") {
+  test_nn_impl<3, kdtree::container::layout::row_major, 1 << 6>();
+  test_nn_impl<3, kdtree::container::layout::col_major, 1 << 6>();
+}
+
+TEST_CASE("[random][case=1] kdtree::nn dim=8 n=1<<8") {
+  test_nn_impl<8, kdtree::container::layout::row_major, 1 << 8>();
+  test_nn_impl<8, kdtree::container::layout::col_major, 1 << 8>();
+}
+
+TEST_CASE("[random][case=1] kdtree::nn dim=16 n=512") {
+  test_nn_impl<16, kdtree::container::layout::row_major, 1 << 9>();
+  test_nn_impl<16, kdtree::container::layout::col_major, 1 << 9>();
+}
+
+TEST_CASE("[random][case=1] kdtree::nn multiple dims/n") {
+
+  SUBCASE("dim=8, n=1024") {
+    test_nn_impl<8, kdtree::container::layout::row_major, 1 << 10>();
+    test_nn_impl<8, kdtree::container::layout::col_major, 1 << 10>();
+  }
+
+  SUBCASE("dim=16, n=512") {
+    test_nn_impl<16, kdtree::container::layout::row_major, 1 << 9>();
+    test_nn_impl<16, kdtree::container::layout::col_major, 1 << 9>();
+  }
+
+}
+
